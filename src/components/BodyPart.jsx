36b6b3150c453b8e8fa72e0f91a3dcc0df5df7e5
Ji Bing Ni <112521325+JiBing17@users.noreply.github.com>
2023-11-26 14:35:36 -0500

    comments added

--- src/components/BodyPart.jsx
+++ src/components/BodyPart.jsx
@@ -2,23 +2,51 @@ import React from 'react';
 import { Stack, Typography } from '@mui/material';
 import Icon from '../assets/icons/gym.png';
 
-// BodyPart Component
+// BodyPart Component: Represents a selectable body part card
 const BodyPart = ({ item, setBodyPart, bodyPart }) => (
   <Stack
     type="button"
     alignItems="center"
     justifyContent="center"
     className="bodyPart-card"
-    // top red border for selected body part
-    sx={bodyPart === item ? { borderTop: '4px solid #FF2625', background: '#fff', borderBottomLeftRadius: '20px', width: '270px', height: '282px', cursor: 'pointer', gap: '47px' } : { background: '#fff', borderBottomLeftRadius: '20px', width: '270px', height: '282px', cursor: 'pointer', gap: '47px' }}
-    // set clicked bodypart component to targeted body part
+    // Add top red border for the selected body part
+    sx={
+      bodyPart === item
+        ? {
+            borderTop: '4px solid #FF2625',
+            background: '#fff',
+            borderBottomLeftRadius: '20px',
+            width: '270px',
+            height: '282px',
+            cursor: 'pointer',
+            gap: '47px',
+          }
+        : {
+            background: '#fff',
+            borderBottomLeftRadius: '20px',
+            width: '270px',
+            height: '282px',
+            cursor: 'pointer',
+            gap: '47px',
+          }
+    }
+    // Set clicked body part component to the targeted body part
     onClick={() => {
       setBodyPart(item);
+      // Scroll to a specific position when a body part is clicked
       window.scrollTo({ top: 1800, left: 100, behavior: 'smooth' });
     }}
   >
     <img src={Icon} alt="dumbbell" style={{ width: '40px', height: '40px' }} />
-    <Typography fontSize="24px" fontWeight="bold" fontFamily="Alegreya" color="#3A1212" textTransform="capitalize">{item}</Typography>
+    <Typography
+      fontSize="24px"
+      fontWeight="bold"
+      fontFamily="Alegreya"
+      color="#3A1212"
+      textTransform="capitalize"
+    >
+      {item}
+    </Typography>
   </Stack>
 );
 

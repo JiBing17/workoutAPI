4957e058c9fdb8ab648498e0ab66b056af7c75e6
Ji Bing Ni <112521325+JiBing17@users.noreply.github.com>
2023-11-26 14:39:46 -0500

    added comments

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -5,11 +5,11 @@ import BodyPartImage from '../assets/icons/body-part.png';
 import TargetImage from '../assets/icons/target.png';
 import EquipmentImage from '../assets/icons/equipment.png';
 
-// Detail Component 
+// Detail Component: Renders detailed exercise information
 const Detail = ({ exerciseDetail }) => {
   const { bodyPart, gifUrl, name, target, equipment } = exerciseDetail;
 
-  // array of objects that contain icon and name for iteration purposes
+  // Array of objects containing icon and name for iteration purposes
   const extraDetail = [
     {
       icon: BodyPartImage,
@@ -26,24 +26,32 @@ const Detail = ({ exerciseDetail }) => {
   ];
 
   return (
+    // Main container with exercise details
     <Stack gap="60px" sx={{ flexDirection: { lg: 'row' }, p: '20px', alignItems: 'center' }}>
+      {/* Exercise image */}
       <img src={gifUrl} alt={name} loading="lazy" className="detail-image" />
+      {/* Stack for text information */}
       <Stack sx={{ gap: { lg: '35px', xs: '20px' } }}>
+        {/* Exercise name */}
         <Typography sx={{ fontSize: { lg: '64px', xs: '30px' } }} fontWeight={700} textTransform="capitalize">
           {name}
         </Typography>
+        {/* Exercise description */}
         <Typography sx={{ fontSize: { lg: '24px', xs: '18px' } }} color="#4F4C4C">
           Exercises keep you strong.{' '}
           <span style={{ textTransform: 'capitalize' }}>{name}</span> is one
-          of the best <br /> exercises to target your {target}. It will help you improve your{' '}
+          of the best <br /> exercises to target your {target}. It will help you improve your {' '}
           <br /> mood and gain energy.
         </Typography>
-        {/*Iterates through each object in extra detail and displays it's picture with its corresponding info*/}
+        {/* Iterate through each object in extraDetail and display its picture with corresponding info */}
         {extraDetail?.map((item) => (
           <Stack key={item.name} direction="row" gap="24px" alignItems="center">
+            {/* Button container for icon */}
             <Button sx={{ background: '#FFF2DB', borderRadius: '50%', width: '100px', height: '100px' }}>
+              {/* Icon */}
               <img src={item.icon} alt={bodyPart} style={{ width: '50px', height: '50px' }} />
             </Button>
+            {/* Display extra detail */}
             <Typography textTransform="capitalize" sx={{ fontSize: { lg: '30px', xs: '20px' } }}>
               {item.name}
             </Typography>
--- src/components/ExerciseCard.jsx
+++ src/components/ExerciseCard.jsx
@@ -2,18 +2,24 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button, Stack, Typography } from '@mui/material';
 
-// Exercise Card Component 
+// Exercise Card Component: Represents an individual exercise card
 const ExerciseCard = ({ exercise }) => (
+  // Link to the exercise detail page based on exercise ID
   <Link className="exercise-card" to={`/exercise/${exercise.id}`}>
+    {/* Exercise GIF */}
     <img src={exercise.gifUrl} alt={exercise.name} loading="lazy" />
+    {/* Stack for buttons and exercise name */}
     <Stack direction="row">
+      {/* Button for exercise body part */}
       <Button sx={{ ml: '21px', color: '#fff', background: '#FFA9A9', fontSize: '14px', borderRadius: '20px', textTransform: 'capitalize' }}>
         {exercise.bodyPart}
       </Button>
+      {/* Button for exercise target */}
       <Button sx={{ ml: '21px', color: '#fff', background: '#FCC757', fontSize: '14px', borderRadius: '20px', textTransform: 'capitalize' }}>
         {exercise.target}
       </Button>
     </Stack>
+    {/* Exercise name */}
     <Typography ml="21px" color="#000" fontWeight="bold" sx={{ fontSize: { lg: '24px', xs: '20px' } }} mt="11px" pb="10px" textTransform="capitalize">
       {exercise.name}
     </Typography>

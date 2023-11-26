ab4d1059160242e4d8c71b0c12fbcda189ed14d8
Ji Bing Ni <112521325+JiBing17@users.noreply.github.com>
2023-11-26 14:47:34 -0500

    added comments

--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -3,28 +3,28 @@ import { Box, Button, Stack, TextField, Typography } from '@mui/material';
 import { exerciseOptions, fetchData } from '../utils/fetchData';
 import HorizontalScrollbar from './HorizontalScrollbar';
 
-// Search Exercises Component
+// Search Exercises Component: Component for searching and filtering exercises
 const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
 
+  // State for search input and body parts list
   const [search, setSearch] = useState('');
   const [bodyParts, setBodyParts] = useState([]);
 
-  // runs at start when component is mounted to fetch bodypart data for display
+  // Fetch body part data when component mounts
   useEffect(() => {
     const fetchExercisesData = async () => {
-
       const bodyPartsData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', exerciseOptions);
       setBodyParts(['all', ...bodyPartsData]);
     };
     fetchExercisesData();
   }, []);
 
-  // runs when search button is clicked (fetches filtered exercise data related to target)
+  // Handle search button click - filtering exercises based on search criteria
   const handleSearch = async () => {
     if (search) {
       const exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
       
-      // goes through each individual item and checks for similar words in each category
+      // Filter exercises based on name, target, equipment, and body part
       const searchedExercises = exercisesData.filter(
         (item) => item.name.toLowerCase().includes(search)
                || item.target.toLowerCase().includes(search)
@@ -33,34 +33,36 @@ const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
       );
         
       window.scrollTo({ top: 1800, left: 100, behavior: 'smooth' });
-      // reset search 
+      // Reset search input
       setSearch('');
-      // sets exercises to related search exercises 
+      // Set exercises to the filtered/searched exercises
       setExercises(searchedExercises);
     }
   };
 
   return (
-    <Stack alignItems = "center" mt = "37px" justifyContent = "center" p = "20px">
-      <Typography fontWeight = {700} sx = {{ fontSize: { lg: '44px', xs: '30px' } }} mb = "49px" textAlign = "center">
+    <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
+      {/* Title */}
+      <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '30px' } }} mb="49px" textAlign="center">
         Awesome Exercises You <br /> Should Know
       </Typography>
-      <Box position = "relative" mb = "72px">
+      <Box position="relative" mb="72px">
         {/* Search Bar */}
         <TextField
-          height = "76px"
-          sx = {{ input: { fontWeight: '700', border: 'none', borderRadius: '4px' }, width: { lg: '1170px', xs: '350px' }, backgroundColor: '#fff', borderRadius: '40px' }}
-          value = {search}
-          onChange = {(e) => setSearch(e.target.value.toLowerCase())}
-          placeholder = "Search Exercises"
-          type = "text"
+          height="76px"
+          sx={{ input: { fontWeight: '700', border: 'none', borderRadius: '4px' }, width: { lg: '1170px', xs: '350px' }, backgroundColor: '#fff', borderRadius: '40px' }}
+          value={search}
+          onChange={(e) => setSearch(e.target.value.toLowerCase())}
+          placeholder="Search Exercises"
+          type="text"
         />
         {/* Search Button */}
-        <Button className = "search-btn" sx = {{ bgcolor: '#FF2625', color: '#fff', textTransform: 'none', width: { lg: '173px', xs: '80px' }, height: '56px', position: 'absolute', right: '0px', fontSize: { lg: '20px', xs: '14px' } }} onClick = {handleSearch}>
+        <Button className="search-btn" sx={{ bgcolor: '#FF2625', color: '#fff', textTransform: 'none', width: { lg: '173px', xs: '80px' }, height: '56px', position: 'absolute', right: '0px', fontSize: { lg: '20px', xs: '14px' } }} onClick={handleSearch}>
           Search
         </Button>
       </Box>
       <Box sx={{ position: 'relative', width: '100%', p: '20px' }}>
+        {/* Horizontal Scrollbar for body parts */}
         <HorizontalScrollbar data={bodyParts} bodyParts setBodyPart={setBodyPart} bodyPart={bodyPart} />
       </Box>
     </Stack>
--- src/components/SimilarExercises.jsx
+++ src/components/SimilarExercises.jsx
@@ -3,7 +3,7 @@ import { Typography, Box, Stack } from '@mui/material';
 import HorizontalScrollbar from './HorizontalScrollbar';
 import Loader from './Loader';
 
-// Similar Exercises Component
+// Similar Exercises Component: Displays similar exercises based on target muscle and equipment
 const SimilarExercises = ({ targetMuscleExercises, equipmentExercises }) => (
   <Box sx={{ mt: { lg: '100px', xs: '0px' } }}>
 
@@ -11,7 +11,9 @@ const SimilarExercises = ({ targetMuscleExercises, equipmentExercises }) => (
     <Typography sx={{ fontSize: { lg: '44px', xs: '25px' }, ml: '20px' }} fontWeight={700} color="#000" mb="33px">
       Similar <span style={{ color: '#FF2625', textTransform: 'capitalize' }}>Target Muscle</span> exercises
     </Typography>
+    {/* Display similar exercises for the target muscle */}
     <Stack direction="row" sx={{ p: 2, position: 'relative' }}>
+      {/* Display HorizontalScrollbar if exercises for the target muscle are available, otherwise display Loader */}
       {targetMuscleExercises.length !== 0 ? <HorizontalScrollbar data={targetMuscleExercises} /> : <Loader/>}
     </Stack>
 
@@ -19,7 +21,9 @@ const SimilarExercises = ({ targetMuscleExercises, equipmentExercises }) => (
     <Typography sx={{ fontSize: { lg: '44px', xs: '25px' }, ml: '20px', mt: { lg: '100px', xs: '60px' } }} fontWeight={700} color="#000" mb="33px">
       Similar <span style={{ color: '#FF2625', textTransform: 'capitalize' }}>Equipment</span> exercises
     </Typography>
+    {/* Display similar exercises for the equipment */}
     <Stack direction="row" sx={{ p: 2, position: 'relative' }}>
+      {/* Display HorizontalScrollbar if exercises for the equipment are available, otherwise display Loader */}
       {equipmentExercises.length !== 0 ? <HorizontalScrollbar data={equipmentExercises} /> : <Loader/>}
     </Stack>
   </Box>
